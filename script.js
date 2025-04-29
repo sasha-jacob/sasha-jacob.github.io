04babca67cb4780cfb3781a5fd0b8c279cd05cc2
sasha-jacob <sj4703@nyu.edu>
2025-04-29 18:01:15 -0400

    messages

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
-const Days = document.getElementById('days');
-const Hours = document.getElementById('hours');
-const Minutes = document.getElementById('minutes');
-const Seconds = document.getElementById('seconds');
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+const reset = document.getElementById("reset");
+const timer = document.getElementById("timer");
 
-const targetDate = new Date("November 1 2024 00:00:00").getTime();
+let timeleft = 1500; //25mintimes60
+let interval;
+const updateTimer = () => {
+    const minutes = Math.floor(timeLeft/60);
+    const seconds = timeLeft % 60;
 
-function timer () {
-    const currentDate = new Date().getTime();
-    const distance = targetDate - currentDate;
+    timer.innerHTML = `${minutes.toString().padStart(2,"0")}:${seconds.toString().padStart(2,"0")}`;
 
-    const days = Math.floor(distance / 1000 / 60 / 60 / 24);
-    const hours = Math.floor(distance / 1000 / 60 / 60) % 24;
-    const minutes = Math.floor(distance / 1000 / 60) % 60;
-    const seconds = Math.floor(distance / 1000) % 60;
-
-    Days.innerHTML = days;
-    Hours.innerHTML = hours;
-    Minutes.innerHTML = minutes;
-    Seconds.innerHTML = seconds;
-
-    if(distance < 0){
-        Days.innerHTML = "00";
-        Hours.innerHTML = "00";
-        Minutes.innerHTML = "00";
-        Seconds.innerHTML = "00";
-    }
 }
 
-setInterval(timer, 1000);
+const startTimer = () => {
+    interval = setInterval(() => {
+        timeLeft--;
+        updateTimer();
+
+        if(timeLeft === 0){
+            clearInterval(interval);
+            alert("time's up!");
+            timeLeft = 1500;
+            updateTimer();
+        }
+    }, 1000);
+}
+
+const stopTimer = () => clearInterval(interval);
+
+const resetTimer = () => {
+    clearInterval(interval);
+    timeLeft = 1500;
+    updateTimer();
+}
+
+start.addEventListener("click", startTimer);
+stop.addEventListener("click", stopTimer);
+reset.addEventListener("click", resetTimer);
